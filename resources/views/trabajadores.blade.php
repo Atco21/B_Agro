6430a680ecb4cf73c4f1d75bb7af71352decafe2
AlfredCom05 <alfred.comanescu@nunsys.com>
2025-03-05 19:04:49 +0100

    vcvc

--- resources/views/trabajadores.blade.php
+++ resources/views/trabajadores.blade.php
@@ -80,6 +80,44 @@ function inicio() {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const cuadros = document.querySelectorAll(".cuadroPersona");
+    cuadros.forEach(cuadro => {
+        cuadro.addEventListener("click", function () {
+
+            console.log(user.id);
+
+            @foreach ($users as $user)
+                console.log($user)
+                if(user.id == $user.id){}
+
+
+            @endforeach
+
+            // const nombre = user.querySelector("h4.card-title").innerText;
+            // const explotacion = user.querySelector(".card-body h5:nth-child(1) b").innerText;
+            // const rol = user.querySelector(".card-body h5:nth-child(2) b").innerText;
+            // const dni = user.getAttribute("data-dni");
+            // const email = user.getAttribute("data-email");
+            // const fechaNacimiento = user.getAttribute("data-fecha_nacimiento");
+            // const telefono = user.getAttribute("data-telefono");
+            // const usuario = user.getAttribute("data-usuario");
+
+            // document.getElementById("edit_nombre").value = nombre;
+            // document.getElementById("edit_explotacion_id").value = explotacion;
+            // document.getElementById("edit_rol").value = rol;
+            // document.getElementById("edit_dni").value = dni;
+            // document.getElementById("edit_email").value = email;
+            // document.getElementById("edit_fecha_nacimiento").value = fechaNacimiento;
+            // document.getElementById("edit_telefono").value = telefono;
+            // document.getElementById("edit_usuario").value = usuario;
+
+            let modal = new bootstrap.Modal(document.getElementById("editarUsuario"));
+            modal.show();
+        });
+    });
+});
+
 
 
 </script>
@@ -112,7 +150,7 @@ function inicio() {
         @foreach ($users as $user)
 
 
-        <div class="card mt-3 ms-3 ms-4 cuadroPersona" style="width: 25em; height: 20em;" >
+        <div class="card mt-3 ms-3 ms-4 cuadroPersona" style="width: 25em; height: 20em;" id="{{ $user->id }}">
             <div class="d-flex flex-row mt-3 ms-3 align-items-center">
                 <img src="{{asset('./assets/logoAgro.png')}}" alt="Foto de perfil" class="fotoPerfil" width="150px">
                 <h4 class="card-title ps-5">{{$user->nombre}}</h4>
@@ -129,7 +167,102 @@ function inicio() {
 
         @endforeach
 
-    </div>
+
+        <div class="modal" id="editarUsuario" tabindex="-1" aria-labelledby="editarUsuarioModal" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="modal-title">Editar usuario</h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <form action="{{ route('trabajadores.update') }}" method="POST">
+                            @csrf
+                            @method('PUT')
+
+                            <!-- Campo oculto para el ID del usuario -->
+                            <input type="hidden" id="edit_user_id" name="user_id">
+
+                            <div class="pb-5">
+                                <h2 class="mb-3">1. Datos personales</h2>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label for="nombre" class="form-label">Nombre completo:</label>
+                                        <input type="text" class="form-control" id="edit_nombre" name="nombre" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="dni" class="form-label">DNI:</label>
+                                        <input type="text" class="form-control" id="edit_dni" name="dni" placeholder="Introduce DNI" required>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-6">
+                                        <label for="email" class="form-label">Email:</label>
+                                        <input type="email" class="form-control" id="edit_email" name="email" placeholder="Introduce email" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="row">
+                                            <div class="col">
+                                                <label for="fecha_nacimiento" class="form-label">Fecha de nacimiento:</label>
+                                                <input type="date" class="form-control" id="edit_fecha_nacimiento" name="fecha_nacimiento" required>
+                                            </div>
+                                            <div class="col">
+                                                <label for="telefono" class="form-label">Número de teléfono:</label>
+                                                <input type="text" class="form-control" id="edit_telefono" name="telefono" placeholder="Introduce teléfono" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="pb-5">
+                                <h2 class="mb-3">2. Tipo de empleado</h2>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label for="rol" class="form-label">Rol:</label>
+                                        <select class="form-select form-control" id="edit_rol" name="rol" required>
+                                            <option selected disabled>Selecciona una opción</option>
+                                            <option value="jefe de campo">Jefe de campo</option>
+                                            <option value="aplicador">Aplicador</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="explotacion_id" class="form-label">Explotación:</label>
+                                        <select class="form-select form-control" id="edit_explotacion_id" name="explotacion_id" required>
+                                            <option selected disabled>Selecciona una opción</option>
+                                            @foreach ($explotacion as $explo)
+                                                <option value="{{ $explo->id }}">{{ $explo->nombre }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="pb-5">
+                                <h2 class="mb-3">3. Registro</h2>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label for="usuario" class="form-label">Usuario:</label>
+                                        <input type="text" class="form-control" id="edit_usuario" name="usuario" placeholder="Introduce usuario" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="password" class="form-label">Contraseña:</label>
+                                        <input type="password" class="form-control" id="password" name="password" placeholder="Introduce contraseña" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     @else
 
--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,18 @@ use App\Http\Controllers\MaquinaController;
 use App\Http\Controllers\AuthController;
 
 
-Route::get('/login', function(){
+Route::get('/', function(){
     return view('layouts.login');
 });
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 
+
 Route::get('/home', function () {
     return view('layouts.app');
 });
+
 
 
 
@@ -33,6 +35,38 @@ Route::group(['middleware' => 'admin'], function(){
     Route::get('/explotaciones', [Explotacioncontroller::class, 'index'])->name('explotaciones');
     Route::get('/explotaciones/editar', [Explotacioncontroller::class, 'editar'])->name('explotaciones');
 
+    Route::get('/trabajadores', [TrabajadorController::class, 'index'])->name('trabajadores');
+
+    Route::post('/trabajadores', [TrabajadorController::class, 'index'])->name('trabajadores.update');
+
+
+
+    Route::get('/informes', function () {
+        return view('informes');
+    });
+
+    Route::get('/explotaciones/general', [ExplotacionController::class, 'general'])->name('explotaciones.general');
+    Route::get('/explotaciones/parcelas', [parcelasController::class, 'listarParcelasPorExplotacion'])->name('parcelas.listar');
+    Route::get('/explotaciones/ordenes', [ExplotacionController::class, 'ordenes'])->name('explotaciones.ordenes');
+    Route::get('/explotaciones/incidencias', [ExplotacionController::class, 'incidencias'])->name('explotaciones.inciendias');
+    Route::get('/explotaciones/maquinas', [ExplotacionController::class, 'maquinas'])->name('explotaciones.maquinas');
+
+    Route::get('/explotaciones/pedidos', [explotacionController::class, 'pedidos'])->name('explotaciones.pedidos');
+
+
+    Route::get('/explotaciones/parcelas/{id}',[parcelasController::class, 'listarParcelasPorExplotacion']);
+
+
+
+
+    Route::get('/explotaciones/parcelas/{idExplotacion}/{idParcela}/rendimiento', [rendController::class, 'index' ]);
+
+    Route::get('/maquinas', [MaquinaController::class, 'index'])->name('maquinas');
+    Route::post('/maquinas', [MaquinaController::class, 'store'])->name('maquinas.store');
+
+
+    Route::get('/explotaciones/maquinas/{id}', [MaquinaController::class, 'mostrarMaquinasPorExplotacion']);
+    Route::get('/explotaciones/maquinas/{id}', [MaquinaController::class, 'listarParcelasPorExplotacion']);
 
     // return view('explotacion', compact('explotacion'));
 
@@ -44,33 +78,8 @@ Route::group(['middleware' => 'admin'], function(){
 
 //Route::get('/explotaciones', [TrabajadorController::class, 'index'])->name('explotaciones');
 
-Route::get('/trabajadores', [TrabajadorController::class, 'index'])->name('trabajadores');
-
-
-Route::get('/informes', function () {
-    return view('informes');
-});
-
-
-Route::get('/explotaciones/general', [ExplotacionController::class, 'general'])->name('explotaciones.general');
-Route::get('/explotaciones/parcelas', [parcelasController::class, 'listarParcelasPorExplotacion'])->name('parcelas.listar');
-Route::get('/explotaciones/ordenes', [ExplotacionController::class, 'ordenes'])->name('explotaciones.ordenes');
-Route::get('/explotaciones/incidencias', [ExplotacionController::class, 'incidencias'])->name('explotaciones.inciendias');
-Route::get('/explotaciones/maquinas', [ExplotacionController::class, 'maquinas'])->name('explotaciones.maquinas');
-
-Route::get('/explotaciones/pedidos', [explotacionController::class, 'pedidos'])->name('explotaciones.pedidos');
-
-
-Route::get('/explotaciones/parcelas/{id}',[parcelasController::class, 'listarParcelasPorExplotacion']);
 
 
 
 
-Route::get('/explotaciones/parcelas/{idExplotacion}/{idParcela}/rendimiento', [rendController::class, 'index' ]);
 
-Route::get('/maquinas', [MaquinaController::class, 'index'])->name('maquinas');
-Route::post('/maquinas', [MaquinaController::class, 'store'])->name('maquinas.store');
-
-
-Route::get('/explotaciones/maquinas/{id}', [MaquinaController::class, 'mostrarMaquinasPorExplotacion']);
-Route::get('/explotaciones/maquinas/{id}', [MaquinaController::class, 'listarParcelasPorExplotacion']);
