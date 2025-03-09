de5b0fbe7e275e26c8ebb65cccb27151255cd08a
Atco21 <alfcom2@alu.edu.gva.es>
2025-03-09 08:58:51 +0100

    cambios en el docker compose

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,3 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    header('Access-Control-Allow-Origin: https://f-agro.pages.dev');
+    header('Access-Control-Allow-Credentials: true');
+
